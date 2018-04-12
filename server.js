0160075abeacc29cfb55626a218e0bd73721b6c8
Hunter Wiley <hunterwiley88@gmail.com>
2018-04-11 20:51:02 -0700

    tablefindr; get yoself a table

---
+++ server.js
@@ -0,0 +1,48 @@
+// npm packages
+var express = require('express')
+var bodyparser = require('body-parser')
+var path = require('path')
+
+// new express app
+var app = express()
+
+var data = []
+// middleware
+app.use(express.static(path.join(__dirname, 'public/views')))
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
+
+// your code here...
+app.get('/', function(req, res){
+  res.sendFile('/index.html', {root: __dirname})
+})
+app.get('/reservation', function(req, res){
+  res.redirect('./reservation.html')
+})
+app.get('/tables', function(req, res){
+  res.redirect('./tables.html')
+})
+
+
+
+app.post('/post', function (req, res) {
+data.push(req.body)
+// console.log(data)
+})
+
+
+
+app.get('/getData', function(req, res){
+  // res.json(data)
+  res.send(data)
+  console.log(data)
+})
+
+
+
+
+var PORT = process.env.PORT || 3000
+// listening port
+app.listen(PORT, function (e) {
+  if (e) throw e
+})
